640381f734b681a4f7d9cf599b334f0d6ea4008e
Ganja Vasiliy <vasllly219@gmail.com>
2019-04-07 01:58:23 +0300

    DoneFix

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -4,9 +4,19 @@ import readGameSaving from './readGameSaving';
 
 export default class GameSavingLoader {
   async load() {
-    const data = await readGameSaving(); // возвращается Promise!
-    const save = new GameSavingData(data);
-    const value = await save.json(); // возвращается Promise!
-    return value;
+    try {
+      const data = await readGameSaving(); // возвращается Promise!
+      if (data === undefined) {
+        throw new Error('Данные не получены');
+      }
+      const save = new GameSavingData(data);
+      const value = await save.json(); // возвращается Promise!
+      if (value === undefined) {
+        throw new Error('Данные не получены');
+      }
+      return value;
+    } catch (error) {
+      return 'Данные не получены';
+    }
   }
 }
